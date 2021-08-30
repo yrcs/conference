93e3faa4d0ea27b860973a2ff424898384474640
yrcs <tearon@qq.com>
2021-08-31 00:12:52 +0800

    first commit

---
+++ src/components/HomeOrTutorial.tsx
@@ -0,0 +1,21 @@
+import { connect } from "../data/connect";
+import { Redirect } from "react-router";
+
+interface StateProps {
+  hasSeenTutorial: boolean;
+}
+
+const HomeOrTutorial: React.FC<StateProps> = ({ hasSeenTutorial }) => {
+  return hasSeenTutorial ? (
+    <Redirect to="/tabs/schedule" />
+  ) : (
+    <Redirect to="/tutorial" />
+  );
+};
+
+export default connect<{}, StateProps, {}>({
+  mapStateToProps: (state) => ({
+    hasSeenTutorial: state.user.hasSeenTutorial,
+  }),
+  component: HomeOrTutorial,
+});
